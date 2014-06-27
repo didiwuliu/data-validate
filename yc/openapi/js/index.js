38aa184a5638a3566dd76f9357fe1ed2fd58f912
zhangchunsheng <zhangchunsheng423@gmail.com>
2014-06-28 01:28:41 +0800

    1.新功能：校验价格信息

--- yc/openapi/js/index.js
+++ yc/openapi/js/index.js
@@ -12,29 +12,42 @@ var url = "http://data-validate.yongche.test/index.php?method=all_price";
 
 $(document).ready(function() {
     $("#check").click(function() {
+        $("#wait").html("校验中...");
         $.ajax({
             url: url,
             type: "get",
             success: function(data) {
+                $("#wait").html("正在校验...");
                 var result = JSON.parse(data);
 
                 result = result.result;
                 var price_list = result.price_list;
 
+                var car_type;
+                var car_type_value;
+                var product_list;
+                var product_list_value;
+                var valid_key_array;
+                var flag = true;
+                var valid_value;
+                var product_type_value;
+
+                var log = "";
+
                 for(var key in price_list) {
-                    var car_type = price_list[key];
+                    car_type = price_list[key];
                     for(var car_type_key in car_type) {
-                        var car_type_value = car_type[car_type_key];
+                        car_type_value = car_type[car_type_key];
                         if(typeof car_type_value.name != "undefined"
                             && typeof car_type_value.person_number != "undefined"
                             && typeof car_type_value.order_id != "undefined"
                             && typeof car_type_value.desc != "undefined"
                             && typeof car_type_value.list != "undefined") {
-                            var product_list = car_type_value.list;
+                            product_list = car_type_value.list;
                             for(var product_list_key in product_list) {
-                                var product_list_value = product_list[product_list_key];
+                                product_list_value = product_list[product_list_key];
                                 if(product_list_key == 1) {
-                                    var valid_key_array = [
+                                    valid_key_array = [
                                         "min_fee",
                                         "fee_per_hour",
                                         "fee_per_kilometer",
@@ -44,21 +57,23 @@ $(document).ready(function() {
                                         "granularity",
                                         "cancel_time"
                                     ];
-                                    var flag = true;
+                                    flag = true;
                                     for(var valid_key in valid_key_array) {
-                                        var valid_value = valid_key_array[valid_key];
+                                        valid_value = valid_key_array[valid_key];
                                         if(product_list_value[valid_value] == null) {
                                             flag = false;
-                                            console.log("city:" + key + ",car_type:" + car_type_key + ",product_type:" + product_list_key + ", " + valid_value + " is not set\n");
+                                            log = "city:" + key + ",car_type:" + car_type_key + ",product_type:" + product_list_key + ", " + valid_value + " is not set\n";
+                                            error(log);
                                         }
                                     }
                                     if(flag) {
-                                        console.log("city:" + key + ",car_type:" + car_type_key + ",product_type:" + product_list_key + " is right\n");
+                                        log = "city:" + key + ",car_type:" + car_type_key + ",product_type:" + product_list_key + " is right\n";
+                                        info(log);
                                     }
                                 } else if(product_list_key == 7 || product_list_key == 8 || product_list_key == 11 || product_list_key == 12) {
                                     for(var product_type_key in product_list_value) {
-                                        var product_type_value = product_list_value[product_type_key];
-                                        var valid_key_array = [
+                                        product_type_value = product_list_value[product_type_key];
+                                        valid_key_array = [
                                             "fee",
                                             "min_fee",
                                             "fee_per_hour",
@@ -71,26 +86,43 @@ $(document).ready(function() {
                                             "is_activity",
                                             "granularity"
                                         ];
-                                        var flag = true;
+                                        flag = true;
                                         for(var valid_key in valid_key_array) {
-                                            var valid_value = valid_key_array[valid_key];
+                                            valid_value = valid_key_array[valid_key];
                                             if(product_type_value[valid_value] == null) {
                                                 flag = false;
-                                                console.log("city:" + key + ",car_type:" + car_type_key + ",product_type:" + product_list_key + ", " + valid_value + " is not set\n");
+                                                log = "city:" + key + ",car_type:" + car_type_key + ",product_type:" + product_list_key + ", " + valid_value + " is not set\n";
+                                                error(log);
                                             }
                                         }
                                         if(flag) {
-                                            console.log("city:" + key + ",car_type:" + car_type_key + ",product_type:" + product_list_key + " is right\n");
+                                            log = "city:" + key + ",car_type:" + car_type_key + ",product_type:" + product_list_key + " is right\n";
+                                            info(log);
                                         }
                                     }
                                 }
                             }
                         } else {
-                            console.log("city:" + key, + "car_type:" + car_type_key + " has an error. name,person_number,order_id,desc is not set\n");
+                            log = "city:" + key, + "car_type:" + car_type_key + " has an error. name,person_number,order_id,desc is not set\n";
+                            error(log);
                         }
                     }
                 }
+                $("#wait").html("校验完毕...");
             }
         });
     });
 });
+
+function info(content) {
+    log(content, "green");
+}
+
+function error(content) {
+    log(content, "red");
+}
+
+function log(content, color) {
+    var result = $("#result");
+    result.append($("<li style=\"color:" + color + "\">" + content + "</li>"));
+}

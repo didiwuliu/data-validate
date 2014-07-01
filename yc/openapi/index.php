e375f87a277cd4c4e3eeb59f3fc752a4a0a695f0
zhangchunsheng <zhangchunsheng423@gmail.com>
2014-07-01 19:25:41 +0800

    2.新功能：增加正式服务器配置

--- yc/openapi/index.php
+++ yc/openapi/index.php
@@ -8,40 +8,65 @@
  * @date 2014-06-27
  * @copyright  Copyright (c) 2014-2014 Luomor Inc. (http://www.luomor.com)
  */
-$method = $_GET["method"];
+$method = isset($_GET["method"]) ? $_GET["method"] : "all_price";
+$server = isset($_GET["server"]) ? $_GET["server"] : "test";// release test
 
 if($method == "login") {
-    $result = json_decode(login());
+    $result = json_decode(login($server));
     echo json_encode($result);
 } elseif($method == "all_price") {
-    $login_info = json_decode(login());
+    $login_info = json_decode(login($server));
     $access_token = $login_info->access_token;
-    $result = json_decode(get_all_price($access_token));
+    $result = json_decode(get_all_price($access_token, $server));
     echo json_encode($result);
 } else {
     echo '{"ret_code":200}';
 }
 
-function login() {
-    $url = "http://openapi.yongche.org/oauth2/token.php";
-    $array = array(
-        "grant_type" => "password",
-        "username" => "16811303455",
-        "password" => "111111",
-        "device_token" => "111111",
-        "uuid" => "111111",
-        "macaddress" => "111111",
-    );
-    $header = array(
-        'User-Agent:iWeidao/5.2.2 (iPhone; iOS 7.0.2)',
-        'Accept-Encoding:deflate',
-        'Authorization:Basic dGVzdDp0ZXN0'
-    );
+function login($server) {
+    if($server == "release") {
+        $url = "http://open.yongche.com/oauth2/token.php";
+        $array = array(
+            "grant_type" => "password",
+            "username" => "16846014601",
+            "password" => "123456",
+            "device_token" => "111111",
+            "uuid" => "111111",
+            "macaddress" => "111111",
+        );
+
+        $header = array(
+            'User-Agent:iWeidao/5.2.2 (iPhone; iOS 7.0.2)',
+            'Accept-Encoding:deflate',
+            'Authorization:Basic MTU0NmNhMjdiOGMwYjBiZmM5MGM1Nzg4NDE2ZjBiYzU6NDM2YWY5OGRlZGIwZGZmMjM0NmU1Mzc5MTNhYTlkYzU='
+        );
+    } elseif($server == "test") {
+        $url = "http://openapi.yongche.org/oauth2/token.php";
+        $array = array(
+            "grant_type" => "password",
+            "username" => "16811303455",
+            "password" => "111111",
+            "device_token" => "111111",
+            "uuid" => "111111",
+            "macaddress" => "111111",
+        );
+
+        $header = array(
+            'User-Agent:iWeidao/5.2.2 (iPhone; iOS 7.0.2)',
+            'Accept-Encoding:deflate',
+            'Authorization:Basic dGVzdDp0ZXN0'
+        );
+    }
+
     return request($url, "POST", $array, $header);
 }
 
-function get_all_price($access_token) {
-    $url = "http://openapi.yongche.org/v3/price/all?last_version=0&product_type_id=1%2C7%2C8%2C11%2C12&access_token=$access_token";
+function get_all_price($access_token, $server) {
+    if($server == "release") {
+        $url = "http://open.yongche.com/v3/price/all?last_version=0&product_type_id=1%2C7%2C8%2C11%2C12&access_token=$access_token";
+    } else {
+        $url = "http://openapi.yongche.org/v3/price/all?last_version=0&product_type_id=1%2C7%2C8%2C11%2C12&access_token=$access_token";
+    }
 
     $header = array(
         'User-Agent:iWeidao/5.2.2 (iPhone; iOS 7.0.2)',

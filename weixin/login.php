9141e58aef3fddff0d2bea5fbed5af09e20d908d
zhangchunsheng <zhangchunsheng423@gmail.com>
2014-07-23 20:56:07 +0800

    1.新功能:增加微信测试

---
+++ weixin/login.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @title login
+ * @description
+ * login
+ * @author zhangchunsheng423@gmail.org
+ * @version V1.0
+ * @date 2014-07-23
+ * @copyright  Copyright (c) 2014-2014 Luomor Inc. (http://www.luomor.com)
+ */
+header('Location: https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxb6474ff042f58576&redirect_uri=http%3A%2F%2Fdata-validate.yongche.org%2Fweixin.php&response_type=code&scope=snsapi_base&state=123#wechat_redirect');
+exit();

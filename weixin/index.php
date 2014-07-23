e12d2e9fd91fb750615391172fe4d6dd348c5721
zhangchunsheng <zhangchunsheng423@gmail.com>
2014-07-23 20:19:59 +0800

    1.新功能:增加微信测试

---
+++ weixin/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @title weixin
+ * @description
+ * weixin
+ * @author zhangchunsheng423@gmail.org
+ * @version V1.0
+ * @date 2014-07-23
+ * @copyright  Copyright (c) 2014-2014 Luomor Inc. (http://www.luomor.com)
+ */
+echo "hello";
+
+print_r($_REQUEST);
